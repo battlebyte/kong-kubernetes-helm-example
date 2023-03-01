205cf432c2b5482ac2d417fa9b61f1af6dbf7ad3
battlebyte <jordi@fernandez.name>
2023-03-01 11:25:43 +0100

    User can select domain name. Non-interactive interaction.

--- certs/generateCPendpointCerts.sh
+++ certs/generateCPendpointCerts.sh
@@ -1,21 +1,25 @@
-mkdir ssl-certs/
+#!/bin/bash
 
-cd ssl-certs
+mkdir -p ssl-certs
 
-openssl genrsa -out control-plane-components.key 2048
-openssl req -new -key control-plane-components.key -out control-plane-components.csr
+read -p "Please enter a domain name to use for manager, adminapi, portal, portalapi and api [kong.lan]: " DOMAIN
+DOMAIN=${DOMAIN:-kong.lan}
+echo $DOMAIN
 
-cat > control-plane-components.ext << EOF
+openssl genrsa -out ssl-certs/control-plane-components.key 2048
+openssl req -new -key ssl-certs/control-plane-components.key -out ssl-certs/control-plane-components.csr -subj "/C=US/ST=CA/L=SF/O=Kong/OU=CX/CN=konghq.com"
+
+cat > ssl-certs/control-plane-components.ext << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = manager.example.com
-DNS.2 = adminapi.example.com
-DNS.3 = portal.example.com
-DNS.4 = portalapi.example.com
-DNS.5 = api.example.com
+DNS.1 = manager.$DOMAIN
+DNS.2 = adminapi.$DOMAIN
+DNS.3 = portal.$DOMAIN
+DNS.4 = portalapi.$DOMAIN
+DNS.5 = api.$DOMAIN
 EOF
 
-openssl x509 -req -in control-plane-components.csr -out control-plane-components.crt -days 825 -sha256 -extfile control-plane-components.ext -signkey control-plane-components.key
+openssl x509 -req -in ssl-certs/control-plane-components.csr -CA ca_cert/ca-cert.pem -CAkey ca_cert/ca-cert.key -CAcreateserial -out ssl-certs/control-plane-components.crt -days 825 -sha256 -extfile ssl-certs/control-plane-components.ext
